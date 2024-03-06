e528fd66981006c031dbaeecae4279ddca155a37
MadaoSurmanito <alejandro.sanzhuerta@alum.uca.es>
2024-03-06 09:59:56 +0100

    Revertido el cambio del header

--- js/header.js
+++
@@ -1,9 +0,0 @@
-// Controlar el desplazamiento y cambiar la clase del encabezado
-window.addEventListener('scroll', function() {
-    var header = document.getElementById('header');
-    if (window.scrollY > 50) {
-        header.classList.add('active');
-    } else {
-        header.classList.remove('active');
-    }
-});
